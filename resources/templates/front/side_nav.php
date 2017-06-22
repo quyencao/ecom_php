26d21dfd064f73e6daa84297713987ef114936cd
QuyenCao <boy20195@gmail.com>
2017-06-22 22:51:51 +0700

    Display category from db

--- resources/templates/front/side_nav.php
+++ resources/templates/front/side_nav.php
@@ -1,8 +1,18 @@
 <div class="col-md-3">
     <p class="lead">Shop Name</p>
     <div class="list-group">
-        <a href="category.html" class="list-group-item">Category 1</a>
-        <a href="#" class="list-group-item">Category 2</a>
-        <a href="#" class="list-group-item">Category 3</a>
+        <?php
+            $query = "SELECT cat_title FROM category";
+
+            $result = mysqli_query($connection, $query);
+
+            if(!$result) {
+                die('QUERY FAILED: ' . mysqli_error($connection));
+            }
+
+            while($category = mysqli_fetch_assoc($result)) {
+                echo "<a href='#' class='list-group-item'>{$category['cat_title']}</a>";
+            }
+        ?>
     </div>
 </div>
